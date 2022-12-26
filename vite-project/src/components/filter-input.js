e3a9dba84cc96f0ae54360e2c842c4a0285f93ae
Piotr Machera <piotr.mache@gmail.com>
2022-12-26 16:56:47 +0100

    filter checkobx

--- vite-project/src/components/filter-input.js
+++ vite-project/src/components/filter-input.js
@@ -1,14 +1,50 @@
 import { LitElement, html, css } from "lit";
 import { filterInputCss } from "../templates/templates";
+import data from "../data.json";
 
 export class FilterInput extends LitElement {
+  static get properties() {
+    return {
+      filters: { type: Array },
+    };
+  }
+
   constructor() {
     super();
+    this.filters = data;
   }
+
   static styles = filterInputCss;
 
   render() {
-    return html` <div id="filter"></div>`;
+    let filters_items = [];
+    for (const element of this.filters) {
+      if (
+        !filters_items.includes(
+          ...element.languages,
+          ...element.tools,
+          ...element.level,
+          ...element.role
+        )
+      )
+        filters_items.push(
+          ...element.languages,
+          ...element.tools,
+          element.level,
+          element.role
+        );
+    }
+
+    return html` <div id="filter">
+      ${filters_items.map(
+        (item, index) => html`
+          <div class="filter_checkbox" key=${index}>
+            <input type="checkbox" id=${item} />
+            <label for=${item}>${item}</label>
+          </div>
+        `
+      )}
+    </div>`;
   }
 }
 customElements.define("filter-input", FilterInput);
